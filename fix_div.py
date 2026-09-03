5cdb4b944be897fa93e1f85a1d35e8a8fc9ec03d
Dicky Salim <dickysal1506@gmail.com>
2026-09-03 17:22:15 +0700

    chore: remove leftover helper scripts

--- fix_div.py
+++
@@ -1,7 +0,0 @@
-with open('/Users/yongkroo/Documents/SPARC/central_daily_dashboard/src/pages/CCSalesDashboard.tsx', 'r') as f:
-    content = f.read()
-
-content = content.replace("      </div>\n\n      </div>\n\n    </div>", "      </div>\n\n    </div>")
-
-with open('/Users/yongkroo/Documents/SPARC/central_daily_dashboard/src/pages/CCSalesDashboard.tsx', 'w') as f:
-    f.write(content)
